5f8fd05c3aa3c74bc69738d127d79bf426b7d4bd
Tasnia Sarwar <tasnias@live.unc.edu>
2020-09-27 16:25:23 -0400

    home page styling

--- scripts/sheetsToJSON.js
+++ scripts/sheetsToJSON.js
@@ -4,9 +4,9 @@ async function fetchData(id, num, keepHeader=false) {
     let rows = [];
     let headers = [];
     data['feed']['entry'].forEach((cell) => {
-        rownum = cell['title']['$t'].substring(1) - 1;
-        colnum = cell['title']['$t'].substring(0, 1).charCodeAt(0) - 65;
-        content = isNaN(Number(cell['content']['$t'])) ? cell['content']['$t'].trim() : Number(cell['content']['$t']);
+        let rownum = cell['title']['$t'].substring(1) - 1;
+        let colnum = cell['title']['$t'].substring(0, 1).charCodeAt(0) - 65;
+        let content = isNaN(Number(cell['content']['$t'])) ? cell['content']['$t'].trim() : Number(cell['content']['$t']);
         // Get Headers
         if (rownum == 0) {
             headers.push(content);
